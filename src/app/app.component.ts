7b9484f6b65436325652f609486dd122c5ae2e2e
Ion Fiat <ion.fiat@student.upt.ro>
2024-05-15 12:09:49 +0300

    Topics si imagini

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { TopicsComponent } from './topics/topics.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavBarComponent],
+  imports: [RouterOutlet, NavBarComponent, TopicsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
