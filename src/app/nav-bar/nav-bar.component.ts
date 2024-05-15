9da2a1bd26610cfc14a2f77d3696ccfa09d2a526
Catalin Ionescu <cata.ionescu2003@gmail.com>
2024-05-15 11:27:29 +0300

    nav-bar inceput

---
+++ src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar',
+  standalone: true,
+  imports: [],
+  templateUrl: './nav-bar.component.html',
+  styleUrl: './nav-bar.component.css',
+})
+export class NavBarComponent {
+  logoPath = "../assets/img/logo.png";
+  userPhoto="../assets/img/nouser.jpeg";
+}
