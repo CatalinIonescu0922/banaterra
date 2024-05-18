f8372fdff512c2600a1d9c123d8b75c3182cb8e7
Elvis <elvis.hardalau@student.upt.ro>
2024-05-18 15:35:12 +0300

    Am adaugat butoanele si un citat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { TopicsComponent } from './topics/topics.component';
+import { QuoteComponent } from './quote/quote.component'; 
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RouterLink,CommonModule],
+  imports: [RouterOutlet, NavBarComponent, TopicsComponent, QuoteComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
