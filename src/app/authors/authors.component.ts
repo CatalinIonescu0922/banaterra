c38e92502004ec55f6edc38f1064ad5a2c80e4de
Elvis <elvis.hardalau@student.upt.ro>
2024-05-20 12:38:03 +0300

    Am inclus navbar + merge tot

--- src/app/authors/authors.component.ts
+++ src/app/authors/authors.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { NavBarComponent } from '../nav-bar/nav-bar.component'; 
 @Component({
   selector: 'app-authors',
   standalone: true,
-  imports: [],
+  imports: [NavBarComponent],
   templateUrl: './authors.component.html',
   styleUrl: './authors.component.css'
 })
