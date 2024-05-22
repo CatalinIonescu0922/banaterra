cd45b7b7d85aa465064ea8572038b9890568928f
Catalin Ionescu <cata.ionescu2003@gmail.com>
2024-05-22 16:10:45 +0300

    pagina home aproape completa ca design

--- src/app/home-component/home-component.component.ts
+++ src/app/home-component/home-component.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
+import { ContentComponent } from '../content/content.component';
+import { FooterComponent } from '../footer/footer.component';
 @Component({
   selector: 'app-home-component',
   standalone: true,
-  imports: [NavBarComponent],
+  imports: [NavBarComponent,ContentComponent,FooterComponent],
   templateUrl: './home-component.component.html',
   styleUrl: './home-component.component.css'
 })
