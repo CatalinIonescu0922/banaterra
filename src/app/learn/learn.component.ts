36b85655d2efe5d10a3851b9860fa7683c946647
Cata <cata.ionescu2003@gmail.com>
2024-06-06 17:58:27 +0300

    check

--- src/app/learn/learn.component.ts
+++ src/app/learn/learn.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-learn',
   standalone: true,
-  imports: [],
+  imports: [NavBarComponent,FooterComponent],
   templateUrl: './learn.component.html',
   styleUrl: './learn.component.css'
 })
--- src/app/topics/topics.component.ts
+++ src/app/topics/topics.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { QuoteComponent } from '../quote/quote.component';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
     selector: 'app-topics',
     standalone: true,
     templateUrl: './topics.component.html',
     styleUrls: ['./topics.component.css'],
-    imports: [QuoteComponent, CommonModule,NavBarComponent]
+    imports: [QuoteComponent, CommonModule,NavBarComponent,FooterComponent]
 })
 
 export class TopicsComponent {
