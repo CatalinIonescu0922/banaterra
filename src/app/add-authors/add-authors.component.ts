d2a96fad548de6c7e7d81e539f8323244a950f42
BiancaGenovu <biagenovu10@yahoo.com>
2024-06-06 18:08:13 +0300

    Adaugare autor(doar vizualizare momentan)

---
+++ src/app/add-authors/add-authors.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-authors',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-authors.component.html',
+  styleUrl: './add-authors.component.css'
+})
+export class AddAuthorsComponent {
+
+}
