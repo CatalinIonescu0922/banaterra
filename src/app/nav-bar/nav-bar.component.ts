cae92d12717ea594cf1707ee9d3a6ffeef44dfe8
Cata <cata.ionescu2003@gmail.com>
2024-06-23 20:10:43 +0300

    working login

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { LanguageService } from '../language.service';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
@@ -11,9 +11,12 @@ import { LanguageService } from '../language.service';
 export class NavBarComponent {
   logoPath = '../assets/img/logo.png';
   userPhoto = '../assets/img/nouser.jpeg';
-  constructor(private languageService: LanguageService) { }
+  constructor(private languageService: LanguageService , private router : Router) { }
 
   ChangeLanguage(event: any) {
     this.languageService.changeLanguage(event.target.value);
+  }
+  changeToLogin() :void {
+      this.router.navigate(["/login"]); 
   }
 }
